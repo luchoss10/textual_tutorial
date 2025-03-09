51a9c9395e9ad45af14d7401e254ceeec019562c
luchoss10 <lucncd@gmail.com>
2025-03-08 22:06:32 -0500

    feat: add stopwatch widgets

--- hello.py
+++ hello.py
@@ -1,6 +1,19 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.containers import HorizontalGroup, VerticalScroll
+from textual.widgets import Button, Digits, Footer, Header
 
+class TimeDisplay(Digits):
+    """A widget to display elapsed time."""
+
+class Stopwatch(HorizontalGroup):
+    """A stopwatch widget."""
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets of a stopwatch."""
+        yield Button("Start", id="start", variant="success")
+        yield Button("Stop", id="stop", variant="error")
+        yield Button("Reset", id="reset")
+        yield TimeDisplay("00:00:00.00")
 
 class StopwatchApp(App):
     """A textual app to manage stopwatches."""
@@ -11,6 +24,11 @@ class StopwatchApp(App):
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
+        yield VerticalScroll(
+                Stopwatch(),
+                Stopwatch(),
+                Stopwatch()
+                )
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
